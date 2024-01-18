aafbe38198e76dffee2018205a1090d7b7aa6231
PoyBoy <parvsharma0104@gmail.com>
2024-01-19 04:37:32 +0530

    basic linkupdation

--- entry.py
+++ entry.py
@@ -2,3 +2,6 @@
 # https://pypi.org/project/amazoncaptcha/
 # https://stackoverflow.com/questions/14965306/give-input-to-a-python-script-from-a-shell
 from amazoncaptcha import AmazonCaptcha
+
+# https://github.com/prateeksood/flipkart-auto-checkout-bot-puppeteer
+# https://github.com/kkapuria3/FREE-Amazon-BUY-Bot
