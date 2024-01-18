afd8143377aa8e71d9689f8a841c50bf461b1651
PoyBoy <parvsharma0104@gmail.com>
2024-01-18 13:09:01 +0530

    init commit

---
+++ entry.py
@@ -0,0 +1,4 @@
+# TODO Imp Links
+# https://pypi.org/project/amazoncaptcha/
+# https://stackoverflow.com/questions/14965306/give-input-to-a-python-script-from-a-shell
+from amazoncaptcha import AmazonCaptcha
